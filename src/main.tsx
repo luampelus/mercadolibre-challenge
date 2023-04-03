368104e48332b177a559becd1000de341a163574
luampelus <106559375+luampelus@users.noreply.github.com>
2023-04-03 01:18:16 -0300

    ni idea

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,6 @@ import {ChakraProvider, ColorModeScript} from "@chakra-ui/react";
 import App from "./app";
 import theme from "./theme";
 
-import "./assets/fonts/airbnbcereal-light.woff2"
-import "./assets/fonts/airbnbcereal-regular.woff2"
-import "./assets/fonts/airbnbcereal-semibold.woff2"
-
 ReactDOM.render(
   <>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
